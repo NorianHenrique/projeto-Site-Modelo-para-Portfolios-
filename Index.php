ba2b0c1c335c7645fe2a178a4b6518c6b332db4a
NorianHenrique <NorianHenrique@hotmail.com>
2023-03-18 18:27:32 -0300

    Terceiro

---
+++ Index.php
@@ -0,0 +1,157 @@
+<html>
+    <head>
+        <title>Site Portfolio</title>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,700;1,400&amp;display=swap" rel="stylesheet">
+        <link href="css/Style.css" rel="stylesheet">
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+    </head>
+
+    <body>
+        <header>
+            <div class="logo_nome">
+               <h1>Norian <span>Henrique</span></h1>
+            </div><!--logo_nome-->
+
+            <div class="logo_sobre">
+               <h3>Olá, eu sou</h3>
+               <h2>Norian Henrique, </br> Full-Stack Developer</h2>
+              
+               </h2>
+               <p>Sou um desenvolvedor focado em projetos independentes, nas horas vagas gosto de lutar e dedicar meu tempo a uma vida saudável e minimalista. Veja mais sobre meus projetos abaixo.</p>
+            </div><!--logo-sobre-->
+
+            <div class="clear"></div>
+        </header>
+
+        <section class="trabalhos">
+
+<h2>Trabalhos Recentes</h2>
+<div class="center">
+
+    <div class="trabalho-single">
+
+        <div class="trabalho-img">
+            <!--img-->
+            <img src="Imagens/mockup-macbook.png" />
+        </div>
+
+        <div class="wraper-descricao">
+        <div class="trabalho-descricao">
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare erat eu semper mollis. Ut suscipit sem vitae efficitur posuere. Nunc a eleifend diam. Nam massa leo, vulputate nec ex vel.</p>
+        </div>
+
+        <div class="box-depoimento">
+            <p>“Extremamente profissional, o projeto ficou incrível e completamente dentro das nossas expectativas. Agradecemos profundamente ao Guilherme e sua equipe pelo sucesso."</p>
+            <div class="box-depoimento-user">
+                <img src="Imagens/users.png" />
+                <div class="box-depoimento-nome">
+                    <h2>Luiza</h2>
+                    <h3>CEO do Google</h3>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+    <div class="trabalho-single">
+
+       
+
+        <div class="wraper-descricao">
+        <div class="trabalho-descricao">
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare erat eu semper mollis. Ut suscipit sem vitae efficitur posuere. Nunc a eleifend diam. Nam massa leo, vulputate nec ex vel.</p>
+        </div>
+
+        <div class="box-depoimento">
+            <p>“Extremamente profissional, o projeto ficou incrível e completamente dentro das nossas expectativas. Agradecemos profundamente ao Guilherme e sua equipe pelo sucesso."</p>
+            <div class="box-depoimento-user">
+                <img src="Imagens/users.png" />
+                <div class="box-depoimento-nome">
+                    <h2>Luiza</h2>
+                    <h3>CEO do Google</h3>
+                </div>
+            </div>
+        </div>
+
+        </div>
+
+        <div class="trabalho-img">
+            <!--img-->
+            <img src="Imagens/mockup-macbook.png" />
+        </div>
+    </div>
+</div>
+</section>
+
+         <section class="faq">
+            <div class="center">
+                <h2>Como Posso Te Ajudar?</h2>
+
+                <div class="box-faq">
+                    <h2>Criação de Apps</h2>
+                    <p>Podemos, juntos, criar aplicativos nativos para Android e IOS ou apps híbridos para os dois.</p>
+                </div>
+                <div class="box-faq">
+                    <h2>Criação de Apps</h2>
+                    <p>Podemos, juntos, criar aplicativos nativos para Android e IOS ou apps híbridos para os dois.</p>
+                </div>
+                <div class="box-faq">
+                    <h2>Criação de Apps</h2>
+                    <p>Podemos, juntos, criar aplicativos nativos para Android e IOS ou apps híbridos para os dois.</p>
+                </div>
+                <div class="box-faq">
+                    <h2>Criação de Apps</h2>
+                    <p>Podemos, juntos, criar aplicativos nativos para Android e IOS ou apps híbridos para os dois.</p>
+                </div> 
+                <div style="clear:both;"></div>
+            </div>
+        </section>
+
+
+    <section class="contato">
+    <h2>Entre em Contato</h2>
+       <div class="center">
+          
+        
+    
+        <div class="telefone">
+             <h3>Telefone</h3>
+             <p>(48) 99999999</p>
+             <h3>Telefone</h3>
+             <p>(48) 99999999</p>
+             <h3>Telefone</h3>
+             <p>(48) 99999999</p>
+        </div><!--telefone-->
+
+      
+      
+
+        <div class="formulario">
+             <form method="POST">
+                 <label>Nome</label>
+                 <input type="text" name="nome"/>
+
+                 <label>Email</label>
+                 <input type="email" name="email"/>
+                  
+                 <label>Mensagem</label>
+                 <textarea name="mensagem"></textarea>
+
+                 <input type="submit" name="acao" value="Enviar!"/>
+             </form>
+        </div><!--formulario-->
+
+       
+      
+       </div>
+    </section><!--contato-->
+
+    <footer>
+        <h2>Todos os direitos reservados!</h2>
+    </footer>
+    </body>
+</html>
